98046dd505ad04438c415a7a7b6e20b158e38ed8
Ahmad Al-Sharif <seaskyways@gmail.com>
2018-04-26 13:25:06 +0300

    XML and counter done

--- app/src/main/java/skyapp/ssw/baucounter/MainActivity.kt
+++ app/src/main/java/skyapp/ssw/baucounter/MainActivity.kt
@@ -1,12 +1,27 @@
 package skyapp.ssw.baucounter
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var count = 0
+        set(value) {
+            field = value
+            counterTextView.text = count.toString()
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        countButton.setOnClickListener {
+            count++
+        }
+
+        resetButton.setOnClickListener {
+            count = 0
+        }
     }
 }
